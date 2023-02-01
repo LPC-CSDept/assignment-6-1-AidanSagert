50c30ed142334998f1b33243008f068945b172c5
Aidan Sagert <aidan.sagert@gmail.com>
2023-01-31 19:33:07 -0800

    Removed another space

--- A61.cpp
+++ A61.cpp
@@ -12,7 +12,7 @@ int main ()
     miles = 4276;
     days = 189000;
     cout << "We have made a long journey of " << miles;
-    cout <<" miles. \n";
+    cout <<" miles.\n";
     cout << "Our checking account balance is " << checking;
     cout << "\nAbout " << days << " days ago Columbus ";
     cout << "stood on this spot.\n";
